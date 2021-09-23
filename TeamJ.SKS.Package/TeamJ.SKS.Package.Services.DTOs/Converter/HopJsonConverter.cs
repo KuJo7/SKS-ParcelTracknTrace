b41ecb6ace67d59665b744d933c609b9668b1403
KuJo7 <62607094+KuJo7@users.noreply.github.com>
2021-09-23 19:40:06 +0200

    implemented polymorphism for DTOs

---
+++ TeamJ.SKS.Package/TeamJ.SKS.Package.Services.DTOs/Converter/HopJsonConverter.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamJ.SKS.Package.Services.DTOs.Models;
+
+namespace TeamJ.SKS.Package.Services.DTOs.Converter
+{
+    public class HopJsonConverter : JsonCreationConverter<Hop>
+    {
+        protected override Hop Create(Type objectType, JObject jObject)
+        {
+            if (jObject == null) throw new ArgumentNullException("jObject");
+
+            if (jObject["regionGeoJson"] != null && jObject["numberPlate"] != null)
+            {
+                return new Truck();
+            }
+            else if (jObject["level"] != null && jObject["nextHops"] != null)
+            {
+                return new Warehouse();
+            }
+            else if (jObject["regionGeoJson"] != null && jObject["logisticsPartner"] != null && jObject["logisticsPartnerUrl"] != null)
+            {
+                return new Transferwarehouse();
+            }
+            else
+            {
+                return new Hop();
+            }
+        }
+    }
+}
