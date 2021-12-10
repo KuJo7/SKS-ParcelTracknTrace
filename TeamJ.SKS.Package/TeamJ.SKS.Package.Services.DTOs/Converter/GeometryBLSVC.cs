1eb16b572b39aa1653797b1026b7df8114ecd37d
KuJo7 <62607094+KuJo7@users.noreply.github.com>
2021-12-10 15:41:23 +0100

    added Converter for Geometry

---
+++ TeamJ.SKS.Package/TeamJ.SKS.Package.Services.DTOs/Converter/GeometryBLSVC.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamJ.SKS.Package.Services.DTOs.Converter
+{
+    internal class GeometryBLSVC : ITypeConverter<Geometry, string>, IValueConverter<Geometry, string>
+    {
+        private static readonly JsonSerializer _serializer = GeoJsonSerializer.Create(new(new(), 4326));
+
+        public string Convert(Geometry source, string destination, ResolutionContext context)
+        {
+            return Convert(source, context);
+        }
+
+        public string Convert(Geometry sourceMember, ResolutionContext context)
+        {
+            try
+            {
+                using var stringWriter = new StringWriter();
+                using var jsonWriter = new JsonTextWriter(stringWriter);
+                _serializer.Serialize(jsonWriter, sourceMember);
+                jsonWriter.Flush();
+                stringWriter.Flush();
+                return stringWriter.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to convert Geometry to JSON.", ex);
+            }
+        }
+    }
+}
---
+++ TeamJ.SKS.Package/TeamJ.SKS.Package.Services.DTOs/Converter/GeometrySVCBL.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using NetTopologySuite.Features;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamJ.SKS.Package.Services.DTOs.Converter
+{
+    internal class GeometrySVCBL : ITypeConverter<string, Geometry>, IValueConverter<string, Geometry>
+    {
+        private static readonly JsonSerializer _serializer = GeoJsonSerializer.Create(new(new(), 4326));
+
+        public Geometry Convert(string source, Geometry destination, ResolutionContext context)
+        {
+            return Convert(source, context);
+        }
+
+        public Geometry Convert(string sourceMember, ResolutionContext context)
+        {
+            try
+            {
+                using var stringReader = new StringReader(sourceMember);
+                using var jsonReader = new JsonTextReader(stringReader);
+                return _serializer.Deserialize<Feature>(jsonReader).Geometry;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to convert JSON to Geometry.", ex);
+            }
+        }
+    }
+}
