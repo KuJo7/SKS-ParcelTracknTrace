f7f1b9745d63a619134f1c1f5e3e4eacbdb9d75d
KuJo7 <62607094+KuJo7@users.noreply.github.com>
2022-01-19 14:49:12 +0100

    excluded DTOs

--- TeamJ.SKS.Package/TeamJ.SKS.Package.DataAccess.DTOs/DALWebhookResponse.cs
+++ TeamJ.SKS.Package/TeamJ.SKS.Package.DataAccess.DTOs/DALWebhookResponse.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace TeamJ.SKS.Package.DataAccess.DTOs
 {
+    [ExcludeFromCodeCoverage]
     public class DALWebhookResponse
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
